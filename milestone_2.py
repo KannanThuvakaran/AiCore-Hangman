5f1ec3cfac1adf76ecbafa27a7bfaf683940b832
Kannan <kthuvakaran43@gamil.com>
2023-10-26 20:04:52 +0100

    updates milestone_2.py

--- milestone_2.py
+++ milestone_2.py
@@ -3,15 +3,13 @@ import random
 # Task 1
 fruit_list = ['kiwi', 'strawberry', 'mango', 'banana', 'apple']
 
-
 # Task 2
 guess_fruit = random.choice(fruit_list)
 
-
-'''Task 3'''
+# Task 3
 letter_guess = input('Enter a single letter: ')
 
-'''Task 4'''
+# Task 4
 if len(letter_guess) == 1 and letter_guess.isalpha() == True:
     print('Good guess!')
 else:
